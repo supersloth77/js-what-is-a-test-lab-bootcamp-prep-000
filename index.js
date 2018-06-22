4d154250a4d4c401681aaac70be37c53d341ad06
Peter Traversa <peter.traversa@gmail.com>
2018-06-22 19:02:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // write your code below!
+<<<<<<< HEAD
 var name = "Susan"
+=======
+var name = "Joe"
+>>>>>>> 07efa560305a1bab17937027e6f221aee1273237
 var height = 39
 var message = `${name} is ${height} inches tall`
 
